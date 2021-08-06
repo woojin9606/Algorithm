bd68ff1da82fc55bf95257ab75850fb90f722a10
woojin9606 <qordnwls11@gmail.com>
2021-08-06 15:32:46 +0900

    돌 게임

---
+++ BOJ/num9655.java
@@ -0,0 +1,19 @@
+package algo;
+
+import java.util.Scanner;
+
+public class num9655 {
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a%2==1) {
+			System.out.println("SK");
+		}
+		else {
+				System.out.println("CY");
+		}
+		
+	}
+
+}
