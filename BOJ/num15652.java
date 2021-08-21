1c59926610da6dd20f32de1ac8669029d5bf32a2
woojin9606 <qordnwls11@gmail.com>
2021-08-21 18:43:34 +0900

    N과 M (4)

---
+++ BOJ/num15652.java
@@ -0,0 +1,46 @@
+package algo;
+
+import java.util.Scanner;
+
+public class num15652 {
+	static int N;
+	static int M;
+	static int[] board;
+	static int[] board2;
+	static boolean[] check;
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		N=sc.nextInt();
+		M=sc.nextInt();
+		board=new int[N+1];
+		board2=new int[M+1];
+		check=new boolean[N+1];
+		for(int i=1;i<=N;i++) {
+			board[i]=i;
+		}
+		int cnt=0;
+
+		dfs(0);
+
+
+	}
+	private static void dfs(int cnt) {
+		if(cnt==M) {
+			for (int i = 1; i <= M; i++) {
+				System.out.print(board2[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		cnt++;
+		for (int i = 1; i <= N; i++) {
+			if(!check[i]&&board2[cnt-1]<=i) {
+				board2[cnt]=i;
+				dfs(cnt);
+			}
+		}
+		
+	}
+
+}
